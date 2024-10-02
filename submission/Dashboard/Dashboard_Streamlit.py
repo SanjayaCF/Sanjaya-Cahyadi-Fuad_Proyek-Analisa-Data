b22de88083d6bd7c871b03149ecd7a2cd7a139e6
Sanjaya <117884188+SanjayaCF@users.noreply.github.com>
2024-10-02 20:57:13 +0700

    Update Dashboard_Streamlit.py

--- submission/Dashboard/Dashboard_Streamlit.py
+++ submission/Dashboard/Dashboard_Streamlit.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv('day.csv')
+    df = pd.read_csv('submission/Dashboard/day.csv')
     return df
 
 data = load_data()
